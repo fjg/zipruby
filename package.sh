17c3dbe8a842a959b8f4fdb313caa8ff13392ffd
fjg <fjgrimault@gmail.com>
2013-04-03 16:05:37 +0200

    split package/clean commands

--- package.sh
+++ package.sh
@@ -15,19 +15,13 @@ rdoc -w 4 -SHN -m README.txt README.txt zipruby.c LICENSE.libzip ChangeLog --tit
 mkdir work
 cp -r * work 2> /dev/null
 cd work
-tar jcf zipruby-${VERSION}.tar.bz2 --exclude=.svn README.txt *.gemspec ext doc
+tar jcf zipruby-${VERSION}.tar.bz2 --exclude=.git README.txt *.gemspec ext doc
 gem build zipruby.gemspec
-gem build zipruby-mswin32.gemspec
-gem build zipruby1.8-mswin32.gemspec
-cp zipruby-${VERSION}-x86-mswin32.gem zipruby-${VERSION}-mswin32.gem
+#gem build zipruby-mswin32.gemspec
+#gem build zipruby1.8-mswin32.gemspec
+#cp zipruby-${VERSION}-x86-mswin32.gem zipruby-${VERSION}-mswin32.gem
 rm -rf lib
-mv lib1.9 lib
-gem build zipruby1.9-mswin32.gemspec
-rm zipruby.c
+#mv lib1.9 lib
+#gem build zipruby1.9-mswin32.gemspec
 cp *.gem *.tar.bz2 ..
 cd ..
-for i in `ls libzip-${LIBZIP_VERSION}/*.{c,h}`
-do
-  rm ext/`basename $i`
-done
-rm -rf work
---
+++ package_clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+VERSION=0.3.7
+LIBZIP_VERSION=0.9.3
+
+rm *.gem *.tar.bz2 2> /dev/null
+rm -rf doc 2> /dev/null
+rm zipruby.c 2> /dev/null
+rm -f zipruby-*.tar.bz 2> /dev/null
+rm -f zipruby*.gem 2> /dev/null
+for i in `ls libzip-${LIBZIP_VERSION}/*.{c,h}`
+do
+  rm -f ext/`basename $i`
+done
+rm -rf work 2> /dev/null
